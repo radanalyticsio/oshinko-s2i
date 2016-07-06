dee3d3963f3b58890532c71f806d389ba95c199b
Trevor McKay <tmckay@redhat.com>
2016-07-06 14:10:45 -0400

    Initial commit of oshinko-get-cluster utility
    This small utility uses the specified oshinko rest server to
    check for the existence of the specified cluster in the rest server's
    namespace and creates the cluster if it does not exist.
    
    Currently, cluster size is fixed at 1 master and 3 workers.

---
+++ tools/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -ex
+
+TAG=`git describe --tags --abbrev=0 2> /dev/null | head -n1`
+if [ -z $TAG ]; then
+    TAG='0.0.0'
+fi
+
+APP=oshinko-get-cluster
+
+if [ $1 = build ]; then
+    OUTPUT_FLAG="-o _output/oshinko-get-cluster"
+fi
+
+if [ $1 = test ]; then
+    TARGET=./tests
+    GO_OPTIONS=-v
+else
+    TARGET=./oshinko-get-cluster.go
+fi
+
+# this export is needed for the vendor experiment for as long as go version
+# 1.5 is still in use.
+export GO15VENDOREXPERIMENT=1
+
+godep go $1 $GO_OPTIONS $OUTPUT_FLAG $TARGET
