76c11d2714382f2bb3aff44459c6f42621835272
Trevor McKay <tmckay@redhat.com>
2016-07-26 09:47:54 -0400

    Make the forever loop optional after the spark app (#2)
    * Make the forever loop optional after the spark app
    
    The forever loop is necessary when using a deploymentconfig
    to run a spark app, but not when we use a kubernetes job
    object. Add an optional env var to turn on the forever
    loop, but skip it by default.
    
    * Make the forever loop optional after the spark app
    
    * change the name of the loop guard
    * set the optional value true in the deploymentconfig
    * rename pyspark.json to pysparkdc.json (more templates on the way)

--- common/utils/start.sh
+++ common/utils/start.sh
@@ -22,7 +22,10 @@ else
 fi
 
 # Sleep forever so the process does not complete
-while :
-do
-    sleep 5
-done
+if [ $FROM_DEPLOYMENTCONFIG ]
+then
+    while true
+    do
+        sleep 5
+    done
+fi
