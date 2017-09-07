9e359ef2c89b73fc6b22e05c35344f16759ff276
Trevor McKay <tmckay@redhat.com>
2017-09-07 17:18:57 -0400

    Add a script to generate templates with specific image tags

---
+++ release-templates.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: release-templates.sh VERSION-TAG"
+    exit 1
+fi
+
+TOP_DIR=$(readlink -f `dirname "$0"` | grep -o '.*/oshinko-s2i')
+mkdir -p $TOP_DIR/release_templates
+rm -rf $TOP_DIR/release_templates/*
+
+cp $TOP_DIR/pyspark/pysparkbuild*.json $TOP_DIR/release_templates
+cp $TOP_DIR/java/javabuild*.json $TOP_DIR/release_templates
+cp $TOP_DIR/scala/scalabuild*.json $TOP_DIR/release_templates
+
+sed -r -i "s@(radanalyticsio/radanalytics-.*spark)\"@\1:$1\"@" $TOP_DIR/release_templates/*
+
+echo "Successfully wrote templates to release_templates/ with version tag $1"
+echo
+echo "grep radanalyticsio/radanalytics-.*spark:$1 *"
+echo
+cd $TOP_DIR/; grep radanalyticsio/radanalytics-.*spark:$1 release_templates/*
