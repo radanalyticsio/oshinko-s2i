6308c6c2c22ee6c7fcbb1415a82262296d979049
Trevor McKay <tmckay@redhat.com>
2017-09-29 14:22:12 -0400

    Modify check pods to wait for pods to exist

--- travis-check-pods.sh
+++ travis-check-pods.sh
@@ -5,6 +5,15 @@ oc project default
 
 while true; do
     REG=$(oc get pod -l deploymentconfig=docker-registry --template='{{index .items 0 "status" "phase"}}')
+    if [ "$?" -eq 0 ]; then
+        break
+    fi
+    echo "Waiting for registry pod"
+    sleep 10
+done
+
+while true; do
+    REG=$(oc get pod -l deploymentconfig=docker-registry --template='{{index .items 0 "status" "phase"}}')
     if [ "$?" -ne 0 -o "$REG" == "Error" ]; then
         echo "Registy pod is in error state..."
         exit 1
@@ -12,7 +21,18 @@ while true; do
     if [ "$REG" == "Running" ]; then
         break
     fi
+    sleep 5
 done
+
+while true; do
+    REG=$(oc get pod -l deploymentconfig=router --template='{{index .items 0 "status" "phase"}}')
+    if [ "$?" -eq 0 ]; then
+        break
+    fi
+    echo "Waiting for registry pod"
+    sleep 10
+done
+
 
 while true; do
     REG=$(oc get pod -l deploymentconfig=router --template='{{index .items 0 "status" "phase"}}')
@@ -23,5 +43,6 @@ while true; do
     if [ "$REG" == "Running" ]; then
         break
     fi
+    sleep 5
 done
 echo "Registry and router pods are okay"
