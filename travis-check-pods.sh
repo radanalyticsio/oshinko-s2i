ba02e47f5e9d832f8d326bdcfa5eb341ebb97912
Trevor McKay <tmckay@redhat.com>
2017-09-29 15:02:51 -0400

    Fix typo in travis-check-pods.sh redeploy

--- travis-check-pods.sh
+++ travis-check-pods.sh
@@ -7,7 +7,7 @@ while true; do
     ERR=$(oc get pods | grep docker-registry.*deploy.*Error)
     if [ "$?" -eq 0 ]; then
         echo "registry deploy failed, try again"
-        oc deploy dc docker-registry --latest
+        oc deploy dc/docker-registry --latest
         sleep 5
         continue
     fi
@@ -36,7 +36,7 @@ while true; do
     ERR=$(oc get pods | grep router.*deploy.*Error)
     if [ "$?" -eq 0 ]; then
         echo "router deploy failed, try again"
-        oc deploy dc router --latest
+        oc deploy dc/router --latest
         sleep 5
         continue
     fi
