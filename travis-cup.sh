e4b60f81919a7b109c82afac6ec8b963dcee999e
Trevor McKay <tmckay@redhat.com>
2017-09-29 12:17:31 -0400

    On cluster up retry, login as system:admin

--- travis-cup.sh
+++ travis-cup.sh
@@ -8,6 +8,7 @@ for tries in {1..3}; do
     sudo oc cluster up # --host-config-dir=/home/travis/gopath/src/github.com/radanalyticsio/origin
     if [ "$?" -eq 0 ]; then
         built=true
+        oc login -u system:admin
         break
     fi
     echo "Retrying oc cluster up after failure"
