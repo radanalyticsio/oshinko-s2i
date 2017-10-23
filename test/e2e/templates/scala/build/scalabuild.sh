ccd4f4610d6406eb9316f9e6fbb8f56c3ec03f32
Trevor McKay <tmckay@redhat.com>
2017-10-23 18:54:12 -0400

    Fix template path expression for tests

--- test/e2e/templates/scala/build/scalabuild.sh
+++ test/e2e/templates/scala/build/scalabuild.sh
@@ -4,7 +4,7 @@
 TEST_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"` | grep -o '.*/oshinko-s2i/test/e2e')
 source $TEST_DIR/common
 
-SCALATEMP_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"` | grep -o '.*/oshinko-s2i/templates')
+SCALATEMP_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"` | grep -o '.*/oshinko-s2i')/templates
 
 SCRIPT_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 source $SCRIPT_DIR/../../builddc
