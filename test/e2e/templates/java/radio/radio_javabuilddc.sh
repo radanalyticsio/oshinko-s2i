f5e14360cb3bdd56cebcc5f25b64df14d82f078e
Trevor McKay <tmckay@redhat.com>
2018-07-30 15:41:36 -0400

    Fix manifest_file test to align with test app build
    * name of jar containing manifest with main_class changed
    * main_class recorded based on pom.xml
    * existing run_app can be used with caching of APP_MAIN_CLASS

--- test/e2e/templates/java/radio/radio_javabuilddc.sh
+++ test/e2e/templates/java/radio/radio_javabuilddc.sh
@@ -12,7 +12,7 @@ set_template $RESOURCE_DIR/oshinko-java-spark-build-dc.json
 set_git_uri https://github.com/radanalyticsio/s2i-integration-test-apps
 set_worker_count $S2I_TEST_WORKERS
 set_fixed_app_name java-build
-set_app_main_class org.apache.spark.examples.JavaSparkPi
+set_app_main_class com.mycompany.app.JavaSparkPi
 
 # Need a little preamble here to read the resources.yaml, create the java template, and save
 # it to the resources directory
@@ -63,8 +63,8 @@ test_spark_options
 echo "++ test_no_source_or_image"
 test_no_source_or_image
 
-echo "++ test_app_file java-spark-pi-1.0-SNAPSHOT.jar"
-test_app_file java-spark-pi-1.0-SNAPSHOT.jar
+echo "++ test_app_file java-spark-pi-1.0-SNAPSHOT-jar-with-dependencies.jar"
+test_app_file java-spark-pi-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 echo "++ test_git_ref"
 test_git_ref $GIT_URI 6fa7763517d44a9f39d6b4f0a6c15737afbf2a5a
