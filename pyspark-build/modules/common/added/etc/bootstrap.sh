9bc22a3a1952f9d8cbcd87c666b3f80dbf7d5e54
Trevor McKay <tmckay@redhat.com>
2018-08-27 18:11:28 -0400

    Regenerate build directories (including incomplete images)

--- pyspark-build/modules/common/added/etc/bootstrap.sh
+++ pyspark-build/modules/common/added/etc/bootstrap.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 set -x
 
+
+function handle_term {
+    echo Received a termination signal
+
+    local cnt
+    local killed=1
+    if [ -n "$PID" ]; then
+        echo "Stopping subprocess $PID"
+        kill -TERM $PID
+        for cnt in {1..10}
+        do
+            kill -0 $PID >/dev/null 2>&1
+            if [ "$?" -ne 0 ]; then
+                killed=0
+                break
+            else
+                sleep 1
+            fi
+        done
+        if [ "$killed" -ne 0 ]; then
+            echo Process is still running 10 seconds after TERM, sending KILL
+            kill -9 $PID
+        fi
+        wait $PID
+        echo "Subprocess stopped"
+    fi
+    exit 0
+}
+
 if [[ $@ == *"$STI_SCRIPTS_PATH"* ]]; then
    exec "$@"
 else
-   exec $APP_ROOT/etc/entrypoint.sh "$@"
+   trap handle_term TERM INT
+   $APP_ROOT/etc/entrypoint.sh "$@" &
+   PID=$!
+   wait $PID
 fi
