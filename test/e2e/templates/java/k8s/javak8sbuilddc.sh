a413dd072da781a74fe60c6df81ba028693923d4
Trevor McKay <tmckay@redhat.com>
2018-08-30 15:41:02 -0400

    Add a clusterconfig test for kubernetes native scheduling

--- test/e2e/templates/java/k8s/javak8sbuilddc.sh
+++ test/e2e/templates/java/k8s/javak8sbuilddc.sh
@@ -14,7 +14,7 @@ cp  $JAVATEMP_DIR/javabuilddc.json $RESOURCE_DIR/javabuilddc.json
 fix_template $RESOURCE_DIR/javabuilddc.json radanalyticsio/radanalytics-java-spark $S2I_TEST_IMAGE_JAVA
 set_template $RESOURCE_DIR/javabuilddc.json
 set_git_uri https://github.com/radanalyticsio/s2i-integration-test-apps
-set_worker_count $S2I_TEST_WORKERS
+set_worker_count 1
 set_fixed_app_name java-build
 set_app_main_class com.mycompany.app.JavaSparkPi
 
@@ -28,5 +28,8 @@ test_manifest_file true
 
 echo "++ test_k8s_complete"
 test_k8s_complete
+
+echo "++ test_k8s_config"
+test_k8s_config 3
 
 os::test::junit::declare_suite_end
