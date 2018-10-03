b49ce433286d08b775e5d9b601b3eee5052a1a94
Trevor McKay <tmckay@redhat.com>
2018-10-03 16:56:14 -0400

    Add scripts to work with rad-image from radanalytics.io
    The templates in this repo differ from those hosted at
    radanalytics.io in resources-is.yaml. Additionally, rad-image
    should be usable from this repo without being duplicated.
    Add scripts to:
    
    * download rad-image from radanalytics.io and modify it to support R
    * copy templates to ./templates-is and modify to reference imagestreams
      generated with rad-image

---
+++ get-rad-image.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function usage() {
+    echo "usage: get-rad-image.sh [-hf]"
+    echo
+    echo "Download the rad-image script from https://radanalytics.io and modify it to support the optional R image"
+    echo
+    echo "Options:"
+    echo "  -h      Print this help message"
+    echo "  -f      Force download and overwrite of rad-image"
+}
+
+FORCE=false
+while getopts fh option; do
+    case $option in
+        h)
+            usage
+            exit 0
+            ;;
+        f)
+            FORCE=true
+            ;;
+        *)
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -f rad-image ]; then
+    if [ "$FORCE" == "true" ]; then
+        rm rad-image
+    else
+        echo rad-image is already downloaded, exiting
+        exit 0
+    fi
+fi
+
+#wget https://radanalytics.io/assets/tools/rad-image
+wget https://raw.githubusercontent.com/radanalyticsio/radanalyticsio.github.io/b6a661af7610d8f2bcb46f357e6eafc70a770d50/assets/tools/rad-image -O rad-image
+
+# Change the IMAGESTREAMS variable to include R
+IS=$(grep "IMAGESTREAMS=" rad-image)
+if ! [[ "$IS" = *"radanalytics-r-spark"* ]]; then
+    echo Adding R image to supported images
+    sed -i 's/IMAGESTREAMS="/IMAGESTREAMS="radanalytics-r-spark /' rad-image
+fi
+chmod +x rad-image
---
+++ templates-is.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function usage() {
+    echo "usage: templates-is.sh [-hf] [TAG]"
+    echo
+    echo "Copy s2i to ./templates-is and modify for use with imagestreams created with rad-image"
+    echo
+    echo "Options:"
+    echo "  -h      Print this help message"
+    echo "  -f      Force overwrite of ./templates-is"
+    echo "  TAG     Use TAG as the tag for imagestream references. Default is 'latest'"
+}
+
+FORCE=false
+while getopts fh option; do
+    case $option in
+        h)
+            usage
+            exit 0
+            ;;
+        f)
+            FORCE=true
+            ;;
+        *)
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ "$#" -ne 1 ]; then
+    tag=latest
+else
+    tag=$1
+fi
+
+if [ -d templates-is ]; then
+    if [ "$FORCE" == "true" ]; then
+        rm -rf templates-is/*
+    else
+        echo templates-is already exists, run with '-f' to overwrite
+        exit 1
+    fi
+fi
+
+mkdir -p templates-is
+echo Using tag "'"$tag"'"
+echo Generated:
+templates=$(grep -l DockerImage templates/*.json)
+for t in $templates; do
+    echo "    "templates-is/$(basename $t)
+    cp $t templates-is
+    sed -i 's@"kind": "DockerImage"@"kind": "ImageStreamTag"@g' templates-is/$(basename $t)
+    sed -i -r "s@\"name\": \"radanalyticsio/(.*)\"@\"name\": \"\1:"$tag"@g" templates-is/$(basename $t) 
+done	
+
